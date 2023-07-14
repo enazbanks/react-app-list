6d7058abb70d2bff43ac01e1b31be453679b43f9
enazbanks <enazrbanks@gmail.com>
2023-07-14 18:20:21 +1000

    UI

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
 import "./styles.css"
+import React from "react"
 
 export default function App() {
-  return "hi"
+  return (
+    <>
+    <h1>Todo List:</h1>
+    <ul className= "list">
+      <li>
+        <label>
+          <input type="checkbox" />
+            Item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+    </ul>
+  <form className="new-item-form">
+    <div className="form-row">
+      <label htmlFor="item">New Task:</label>
+      <input type ="text" id="item"></input>
+    </div>
+    <button className="btn"> Add</button>
+  </form>
+  </>
+  )
 }
