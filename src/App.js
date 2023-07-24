340f845232959f88b7ac3c29b74e72b9fccf2c05
enazbanks <enazrbanks@gmail.com>
2023-07-24 12:21:14 +1000

    handle submit setTodos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./styles.css"
 import React from "react"
+import { useState } from "react"
 
 export default function App() {
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState([])
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        {id: crypto.randomUUID(), title: newItem, completed: false},
+      ]
+    })
+  }
+
+  console.log(todos)
+
   return (
     <>
     <h1>Todo List:</h1>
@@ -14,10 +31,11 @@ export default function App() {
         <button className="btn btn-danger">Delete</button>
       </li>
     </ul>
-  <form className="new-item-form">
+  <form onSubmit={handleSubmit} className="new-item-form">
     <div className="form-row">
       <label htmlFor="item">New Task:</label>
-      <input type ="text" id="item"></input>
+      <input type ="text" id="item" value={newItem} onChange={e => setNewItem(e.target.value)}>
+      </input>
     </div>
     <button className="btn"> Add</button>
   </form>
